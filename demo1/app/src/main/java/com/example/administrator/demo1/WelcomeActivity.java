3ea8390b6b95400e54bbdf3793d6d8fdd3077229
ClementLuberry <clement.luberry@gmail.com>
2015-11-17 12:13:46 +0100

    Gestion des joueurs après appui du bouton start

--- demo1/app/src/main/java/com/example/administrator/demo1/WelcomeActivity.java
+++ demo1/app/src/main/java/com/example/administrator/demo1/WelcomeActivity.java
@@ -49,7 +49,7 @@ public class WelcomeActivity extends Activity {
         finish();
     }
 
-
+    //Process behind start button
     public void btStart(View view) throws InterruptedException{
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
