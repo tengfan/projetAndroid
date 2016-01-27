7af94390d6450cb8c49fb0f135fe42bfc11706d9
tengfan_laptop <teng_fan@hotmail.fr>
2016-01-27 11:34:22 +0100

    game 3 prototype

--- demo1/app/src/main/java/com/example/administrator/demo1/GameThreeActivity.java
+++ demo1/app/src/main/java/com/example/administrator/demo1/GameThreeActivity.java
@@ -1,13 +1,138 @@
 package com.example.administrator.demo1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameThreeActivity extends Activity {
+
+    public float xPosition, xAcceleration,xVelocity = 0.0f;
+    public float yPosition, yAcceleration,yVelocity = 0.0f;
+    public float xmax,ymax;
+    private SensorManager sensorManager = null;
+    public float frameTime = 0.666f;
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+
+        //Set FullScreen & portrait
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // Get a reference to a SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener((SensorEventListener) this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME);
+        // setContentView(R.layout.main);
+
+        //Calculate Boundry
+        Display display = getWindowManager().getDefaultDisplay();
+        xmax = (float)display.getWidth() - 50;
+        ymax = (float)display.getHeight() - 50;
         setContentView(R.layout.activity_game_three);
+    }
+
+    // This method will update the UI on new sensor events
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        {
+            if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+                //Set sensor values as acceleration
+                yAcceleration = sensorEvent.values[1];
+                xAcceleration = sensorEvent.values[2];
+                updateBall();
+            }
+        }
+    }
+
+    private void updateBall() {
+
+
+        //Calculate new speed
+        xVelocity += (xAcceleration * frameTime);
+        yVelocity += (yAcceleration * frameTime);
+
+        //Calc distance travelled in that time
+        float xS = (xVelocity/2)*frameTime;
+        float yS = (yVelocity/2)*frameTime;
+
+        //Add to position negative due to sensor
+        //readings being opposite to what we want!
+        xPosition -= xS;
+        yPosition -= yS;
+
+        if (xPosition > xmax) {
+            xPosition = xmax;
+        } else if (xPosition < 0) {
+            xPosition = 0;
+        }
+        if (yPosition > ymax) {
+            yPosition = ymax;
+        } else if (yPosition < 0) {
+            yPosition = 0;
+        }
+    }
+
+    // I've chosen to not implement this method
+    public void onAccuracyChanged(Sensor arg0, int arg1)
+    {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        // Unregister the listener
+        sensorManager.unregisterListener((SensorEventListener) this);
+        super.onStop();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // TODO Auto-generated method stub
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    public class CustomDrawableView extends View
+    {
+        public CustomDrawableView(Context context)
+        {
+            super(context);
+            Bitmap football = BitmapFactory.decodeResource(getResources(), R.drawable.football);
+            final int dstWidth = 50;
+            final int dstHeight = 50;
+            mBitmap = Bitmap.createScaledBitmap(football, dstWidth, dstHeight, true);
+        }
+
+        protected void onDraw(Canvas canvas)
+        {
+            final Bitmap bitmap = mBitmap;
+            canvas.drawBitmap(bitmap, xPosition, yPosition, null);
+            invalidate();
+        }
     }
 }
